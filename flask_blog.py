c306ac6097f115bb2f75058fe829615b5be8f943
Abdulvahab Kharadi <a.kharadi@hrwallingford.com>
2019-10-15 16:53:33 +0100

    Cretaed register and Login forms

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '001786'
+
 p = [
 {
 'name' : "Abdulvahab Kharadi",
@@ -26,6 +29,20 @@ def about():
 @app.route("/persons")
 def persons():
     return render_template("persons.html", persons=p, title='Persons')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Register', form=form)  
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
